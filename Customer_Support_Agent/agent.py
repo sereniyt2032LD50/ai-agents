f039c188c33f2f57a97e2cd13d232599ab9240d4
Aisha Katusi <katusi.aisha@gmail.com>
2026-02-25 20:55:18 +0000

    update to code

--- Customer_Support_Agent/agent.py
+++ Customer_Support_Agent/agent.py
@@ -1,25 +1,44 @@
 from google.adk.agents import Agent
 from google.genai import types 
-from google.genai.models import Litellm
+from google.genai.models.lite_llm import Litellm
+from google.adk.tools import FunctionTool
+import litellm
+from faq import Faq_text
 
-AGENT_MODEL = Litellm(model="openai/gpt-4o-mini")
+AGENT_MODEL = Litellm(model="openai/gpt-4o-mini") #To use a model other tha gemini
 
 
+#Initializing the safety settings for the agent to stay within topic 
 safety_settings = types.SafetySetting (
     category=types.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
     threshold=types.HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
 )
 
-def lookup_faq():
+def lookup_faq(question: str) -> str:
     """ You are a tool that will be used by the customer support agent to answer questions related to the faq """
-    faq_text = "\n".join(f" {k} : {v} for k, v in ")
+    faq_text = "\n".join(f" - {k} : {v} " for k, v in {Faq_text.items()})
 
-    return 
+    prompt = (
+        f"You are helpful assistant. This is an faq database: \n\n{faq_text}\n\n"
+        f"User's question: \"{question}\". "
+        f"Reply with the best match from the faq. If it is not available, say you don't know."
+    )
 
-root_agent = Agent (
+    response = litellm.completion(
+        model="gemini-2.0-flash",
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return response["choices"][0][message][content].strip()
+
+
+faq_tool = FunctionTool(func=lookup_faq)
+
+root_agent = Agent(
     name="CustomerSupportAgent",
     model = "gemini-2.0-flash",
-    description = 
+    description = "An agent that answers from a set of faq's",
+    Instructions =
     """
         You are a customer support agent that answers the user queries from an available faq database. 
         If the user ask questions outside of the faq database, you implement the guardrail strategy
@@ -27,6 +46,8 @@ root_agent = Agent (
 
     generate_content_config=types.GenerateContentConfig(
         temperature=0.2,
-        max_output_tokens=250
-    )
+        max_output_tokens=250,
+        safety_settings=safety_settings
+    ),
+    tools = [faq_tool]
 )
--- Customer_Support_Agent/faq.py
+++ Customer_Support_Agent/faq.py
@@ -1,6 +1,9 @@
 # This file contains faq dictionary which will be used in the lookup faq function
 
-FAQ_TEXT = {
+Faq_text = {
     "Closing hours": "Monday to Friday: 8am - 6pm, Saturday: 9am - 2pm, Sunday: Closed", 
-    "Return Policy":
+    "Return Policy": "After one week of purchase, you can no longer return items. If you within the time limit, please contact support at with the receipt",
+    "Type pf Business" : "Women Own Shop",
+    "Contact Person": "For any inquiries please contact support at the email provided at the contact page",
+    "Time to responds to queries": "The support team will get back to customers within a 48 hour timeframe maximum, but usually responds to queries quicker."
 }
